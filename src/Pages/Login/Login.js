12e43e921665c1c9e68cdac544aa3f8785b4a602
tnayem <tasdidnayem@gmail.com>
2022-11-24 12:25:19 +0600

    login form created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+
+    const handleLogIn = data =>{
+        console.log(data);
+    }
     return (
-        <div>
-            This is Login
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-6'>
+                <h2 className='text-4xl text-center mb-6'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogIn)}>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input {...register("email")} type="email" placeholder="email" className="input input-bordered w-full" />
+                    </div>
+                    <div className="form-control w-full mb-6">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input {...register("password")} type="password" placeholder="password" className="input input-bordered w-full" />
+                    </div>
+                    <input className='btn btn-accent w-full' value='Login' type="submit" />
+                </form>
+                <p>New to Hello.com <Link className='text-primary'>Create New Account</Link></p>
+                <div className="divider">OR</div>
+                <input className='btn btn-success w-full' value='Login With Google' type="submit" />
+            </div>
         </div>
     );
 };
